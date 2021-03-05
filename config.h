95f904534d551a579b8389d69dca0cc32615a420
mgarmuno <miguelgarmm@gmail.com>
2021-03-05 23:33:01 +0100

    configuring dwm keys. added autostart patch

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -19,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "dev", "sys", "www", "mail", "play", "med", "chat", "virt", "tor" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -44,7 +45,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -57,27 +58,75 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "termite", NULL };
+static const char *doom[]     = { "/home/magneto/.emacs.d/bin/doom", "run", NULL };
+static const char *firefox[]  = { "firefox", NULL };
+static const char *spotify[]  = { "spotify", NULL };
+static const char *thunderbird[]  = { "thunderbird", NULL };
+static const char *keepassxc[]  = { "keepassxc", NULL };
+static const char *librewolf[]  = { "librewolf", NULL };
+static const char *librewolfPrivate[]  = { "librewolf", "-private-window", NULL };
+static const char *brave[]  = { "brave", "-private-window", NULL };
+static const char *bravePrivate[]  = { "brave", "-private-window", NULL };
+static const char *chrome[]  = { "google-chorme-stable", NULL };
+static const char *chromium[]  = { "chromium", NULL };
+static const char *vscode[]  = { "vscode", NULL };
+static const char *vim[]  = { "termite", "-e", "vim", NULL };
+static const char *popcorntime[]  = { "popcorntime", NULL };
+static const char *ranger[]  = { "termite", "-e", "ranger", NULL };
+static const char *steam[]  = { "steam-runtime", NULL };
+static const char *lutris[]  = { "lutris", NULL };
+static const char *transmission[]  = { "transmission-gtk", NULL };
+static const char *telegram[]  = { "telegram-desktop", NULL };
+static const char *blueman[]  = { "blueman-manager", NULL };
+static const char *nmdmenu[]  = { "networkmanager_dmenu", NULL };
+static const char *vlc[]  = { "vlc", NULL };
+static const char *pavucontrol[]  = { "pavucontrol", NULL };
+static const char *i3lock[]  = { "sh", "/home/magneto/.config/i3/scripts/.lock.sh", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_w,      spawn,          {.v = librewolf } },
+	{ MODKEY|ShiftMask,             XK_w,      spawn,          {.v = librewolfPrivate } },
+	{ MODKEY|ControlMask,           XK_w,      spawn,          {.v = brave } },
+	{ MODKEY|Mod1Mask,              XK_w,      spawn,          {.v = bravePrivate } },
+	{ MODKEY,                       XK_e,      spawn,          {.v = doom } },
+	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = vscode } },
+	{ MODKEY|ControlMask,           XK_e,      spawn,          {.v = vim } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = ranger } },
+	{ MODKEY,                       XK_t,      spawn,          {.v = transmission } },
+	{ MODKEY|ShiftMask,             XK_t,      spawn,          {.v = telegram } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = keepassxc } },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = pavucontrol } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = spotify } },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = vlc } },
+	{ MODKEY|ControlMask,           XK_s,      spawn,          {.v = popcorntime } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_f,      spawn,          {.v = firefox } },
+	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = chrome } },
+	{ MODKEY|ControlMask,           XK_f,      spawn,          {.v = chromium } },
+	{ MODKEY,                       XK_g,      spawn,          {.v = steam } },
+	{ MODKEY|ShiftMask,             XK_g,      spawn,          {.v = lutris } },
+	{ MODKEY,                       XK_n,      spawn,          {.v = blueman } },
+	{ MODKEY|ShiftMask,             XK_n,      spawn,          {.v = nmdmenu } },
+	{ MODKEY,                       XK_m,      spawn,          {.v = thunderbird } },
+	{ MODKEY,                       XK_b,      spawn,          {.v = i3lock } },
+
+	{ MODKEY,                       XK_v,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_o,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	// { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,             XK_space,  setlayout,      {0} },
+	{ MODKEY,                       XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
