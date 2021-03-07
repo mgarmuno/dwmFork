48c4776e25bbd45dd004bcb05fc2298038d61e3c
mgarmuno <miguelgarmm@gmail.com>
2021-03-07 17:27:57 +0100

    minor config changes

--- config.def.h
+++ config.def.h
@@ -15,8 +15,8 @@ static const int systraypinningfailfirst = 1;
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack:size=10" };
-static const char dmenufont[]       = "Hack:size=10";
+static const char *fonts[]          = { "Hack:size=8" };
+static const char dmenufont[]       = "Hack:size=8";
 static const char col_gray1[]       = "#282A36";
 static const char col_gray2[]       = "#282A36";
 static const char col_gray3[]       = "#F8F8F2";
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "termite", NULL };
 static const char *shutdown[]  = { "shutdown",  "-h", "now", NULL };
 static const char *restart[]  = { "reboot", NULL };
