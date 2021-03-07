f217b2efb06ac5eb14e20b3e174d85a5673a87e5
mgarmuno <miguelgarmm@gmail.com>
2021-03-07 18:41:50 +0100

    font size

--- config.def.h
+++ config.def.h
@@ -15,8 +15,8 @@ static const int systraypinningfailfirst = 1;
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack:size=8" };
-static const char dmenufont[]       = "Hack:size=8";
+static const char *fonts[]          = { "Hack:size=9" };
+static const char dmenufont[]       = "Hack:size=9";
 static const char col_gray1[]       = "#282A36";
 static const char col_gray2[]       = "#282A36";
 static const char col_gray3[]       = "#F8F8F2";
